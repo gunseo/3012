b508ec5baacfd8e4e276383688c1b10aa646e329
박건서 <resiramoo@snu.ac.kr>
2024-10-29 02:30:51 +0900

    서브 페이지 2 컴포넌트까지 완료

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Diary from "./Diary";
+import DiaryPage from "./DiaryPage";
 import "./Archive.css";
 
 function Archive() {
   return (
     <main className="archive">
       <Diary></Diary>
+      <DiaryPage></DiaryPage>
     </main>
   );
 }
---
+++ src/components/DiaryPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./DiaryPage.css";
+
+
+function DiaryPage() {
+  
+  return (
+    <div class="auto-height-box-out">
+        <p className="diarypage-title">오늘의 요리는 뭘까용?</p>
+        <p className="diarypage-date">2024.10.29</p>
+        <div class="auto-height-box-in">This is some textdf that will adjust the height of the div automatically based on the content.</div>
+    </div>
+    
+
+  );
+}
+
+export default DiaryPage;
+
+
+
+
